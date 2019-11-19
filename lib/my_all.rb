e4c9d7e8245710b4eb24b0b2ed8bc27feeea2f85
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-11-19 19:11:04 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,14 @@
 require 'pry'
 
 def my_all?(collection)
-
+  counter = 0
+  all_true = true
+  while counter < collection.length
+    current_true = yield collection[counter]
+    if !current_true
+      all_true = false
+      break
+    end
+    counter += 1
+  end
 end
